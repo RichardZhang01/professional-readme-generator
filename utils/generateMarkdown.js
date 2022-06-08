afeb00a19a2f2691e659c9eb61d1c8327d417856
RZhang <richardzhiyuanzhang@gmail.com>
2022-06-08 10:39:42 -0400

    filled out the renderLicenseLink function. Copy and pasted switch statements to renderLicenseSection function. WIll fill out returns later

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,7 +5,7 @@ function renderLicenseBadge(license) {
   switch (license) {
 
     case 'Apache 2.0':
-      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      return '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
 
     case 'GNU GPLv3':
       return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
@@ -32,15 +32,55 @@ function renderLicenseLink(license) {
 
   switch (license) {
 
+    case 'Apache 2.0':
+      return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
 
+    case 'GNU GPLv3':
+      return '[GPL v3](https://www.gnu.org/licenses/gpl-3.0)';
+
+    case 'MIT':
+      return '[MIT](https://opensource.org/licenses/MIT)';
+
+    case 'ISC':
+      return '[ISC](https://opensource.org/licenses/ISC)';
+
+    case 'GNU GPLv2':
+      return '[GPL v2](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+
+    default:
+      return '';
     
-  }
+  };
 
-}
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+  switch (license) {
+
+    case 'Apache 2.0':
+      return '';
+
+    case 'GNU GPLv3':
+      return '';
+
+    case 'MIT':
+      return '';
+
+    case 'ISC':
+      return '';
+
+    case 'GNU GPLv2':
+      return '';
+
+    default:
+      return '';
+    
+  };
+
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
